a7893f68e4c416322d0f7b7f61dc0a5b9bb57875
AnyaSalova <160308308+AnyaSalova@users.noreply.github.com>
2025-07-05 14:49:56 +0400

    Update StringCheckExample.java

--- task4/StringCheckExample.java
+++ task4/StringCheckExample.java
@@ -4,17 +4,15 @@ import java.util.function.Predicate;
 
 public class StringCheckExample {
     public static void main(String[] args) {
-        // 1. Проверка на null
+
         Predicate<String> isNotNull = s -> s != null;
         System.out.println("Is 'hello' not null? " + isNotNull.test("hello"));
         System.out.println("Is null not null? " + isNotNull.test(null));
-        
-        // 2. Проверка на непустую строку
+
         Predicate<String> isNotEmpty = s -> !s.isEmpty();
         System.out.println("Is 'hello' not empty? " + isNotEmpty.test("hello"));
         System.out.println("Is '' not empty? " + isNotEmpty.test(""));
-        
-        // 3. Комбинированная проверка
+
         Predicate<String> isValid = isNotNull.and(isNotEmpty);
         System.out.println("Is 'hello' valid? " + isValid.test("hello"));
         System.out.println("Is '' valid? " + isValid.test(""));
