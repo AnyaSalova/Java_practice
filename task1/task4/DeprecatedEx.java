7faeb6cfdbf2b874c02a1ecd0874e866ec196acb
AnyaSalova <160308308+AnyaSalova@users.noreply.github.com>
2025-07-05 14:10:17 +0400

    Delete task1/task4 directory

--- task1/task4/DeprecatedEx.java
+++
@@ -1,9 +0,0 @@
-package task4;
-
-import java.lang.annotation.*;
-
-@Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.METHOD, ElementType.TYPE})
-public @interface DeprecatedEx {
-    String message();
-}
--- task1/task4/OldClass.java
+++
@@ -1,9 +0,0 @@
-package task4;
-
-@DeprecatedEx(message = "Use NewClass instead")
-public class OldClass {
-    @DeprecatedEx(message = "Use newMethod() instead")
-    public void oldMethod() {}
-    
-    public void regularMethod() {}
-}
