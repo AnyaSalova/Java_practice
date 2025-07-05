a7a460aaca83574578fff83d29eff09f3c343ea7
AnyaSalova <160308308+AnyaSalova@users.noreply.github.com>
2025-07-05 14:49:31 +0400

    Update AnnotationProcessor.java

--- task4/AnnotationProcessor.java
+++ task4/AnnotationProcessor.java
@@ -6,14 +6,12 @@ public class AnnotationProcessor {
     public static void main(String[] args) {
         Class<?> clazz = OldClass.class;
         
-        // Проверка класса
         if (clazz.isAnnotationPresent(DeprecatedEx.class)) {
             DeprecatedEx annotation = clazz.getAnnotation(DeprecatedEx.class);
             System.out.printf("Внимание: класс '%s' устарел. Альтернатива: '%s'%n",
                 clazz.getSimpleName(), annotation.message());
         }
         
-        // Проверка методов
         for (Method method : clazz.getDeclaredMethods()) {
             if (method.isAnnotationPresent(DeprecatedEx.class)) {
                 DeprecatedEx annotation = method.getAnnotation(DeprecatedEx.class);
